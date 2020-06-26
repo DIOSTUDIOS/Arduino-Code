fec0a04b8b978c0054f3873c99a1ab3b4f5611c9
amos wang <personalmail.wang@icloud.com>
2020-06-26 21:06:05 +0800

    2020.06.26.01

---
+++ exercise/LED.ino
@@ -0,0 +1,16 @@
+const int LED = 13;
+
+void setup()
+{
+	pinMode(LED, OUTPUT);
+
+	digitalWrite(LED, HIGH);
+}
+
+void loop()
+{
+	digitalWrite(LED, LOW);
+	delay(1000);
+	digitalWrite(LED, HIGH);
+	delay(1000);
+}
